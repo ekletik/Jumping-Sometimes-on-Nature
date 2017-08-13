3f261ff84739eb3f6e063d2783d8c67d4133a663
Leo <csneto@L13.local>
2017-08-13 13:42:59 -0400

    Initial commit

---
+++ json.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Leo N. on 2017-08-03.
+ */
+
+var BASEURL = 'https://ekletik.com';
+var content = document.getElementById('gallery');
+
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://ekletik.com/api/portfolio');
+
+// Onload...
+xhr.onload = function () {
+    var data = JSON.parse(xhr.responseText);
+    renderHTML(data);
+}; xhr.send();
+
+function renderHTML(data) {
+    var contentTitle = "";
+    var contentImg   = "";
+    var contentDesc  = "";
+    var contentFull  = "";
+
+    for(var i=0; i < data.length; i++) {
+     if(data[i].autor === "Leo Neto"){
+        contentTitle += data[i].nome;
+        contentFull += '<div class="card">';
+        contentFull += '<img src="' + BASEURL + data[i].capa + '" class="" id="' + i + '">';
+        contentFull += '</div>';
+     }
+    }
+    content.insertAdjacentHTML('beforeend', contentFull);
+}
+
+/*
+<div class="card">
+    <img src="https://unsplash.it/356/280/?image=900" data-tags="Animals,Sea" alt="Animal Seal Lion">
+</div>
+*/
